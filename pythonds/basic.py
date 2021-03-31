1eb521bba5d932bfa8a7de9f750530fc4e28c960
sean galloway <sean.galloway@outlook.com>
2021-03-30 18:57:40 -0700

    added minimal comments

--- pythonds/basic.py
+++ pythonds/basic.py
@@ -1,5 +1,155 @@
-# Stack Class implemented using Python Lists
+# This file contains basic data structure code for education purposes.
+# Use at your own risk.
+
+
+class Node(object):
+    '''
+    ###############################################################################\n
+    # Node Object : This is just a simple node for use in the linked list class\n
+    ###############################################################################\n
+    '''
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList(object):
+    '''
+    ###############################################################################\n
+    # LinkedList Object : This is a versatile linked list object, with only a\n
+    #                     modicum of error checking\n
+    ###############################################################################\n
+    '''
+
+    def __init__(self):
+        # init method : set up the head and tail pointers and initialize the count to 0
+        self.head = None
+        self.tail = None
+        self.list_count = 0
+
+    def __str__(self):
+        # a string representation of the linked list; useful for printing out the
+        # total contents
+        n = self.head
+        s = "Node List [ "
+        while n != None:
+            s += str(n.data) + " "
+            n = n.next
+        s += "]"
+        return s
+
+    def get_count(self):
+        # get_count just returns the count; it's not rocket science
+        return self.list_count
+
+    def peek_head(self):
+        # Return the contents of the head pointer
+        return self.head.data
+
+    def peek_tail(self):
+        # Return the contents of the tail pointer
+        return self.tail.data
+
+    def add_to_head(self, data):
+        # Add the passed in data to the head of the list
+        n = Node(data)
+        self.list_count += 1
+        if self.head == None:
+            self.head = n
+            self.tail = n
+        else:
+            n.next = self.head
+            self.head = n
+
+    def add_to_tail(self, data):
+        # Add the passed in data to the tail of the list
+        n = Node(data)
+        self.list_count += 1
+        if self.head == None:
+            self.head = n
+            self.tail = n
+        else:
+            self.tail.next = n
+            self.tail = n
+
+    def del_from_head(self):
+        # delete the item from the head of the list and return the data
+        if self.list_count > 0:
+            data = self.head.data
+            self.list_count -= 1
+            if self.head.next == None:
+                self.head = None
+                self.tail = None
+            else:
+                n = self.head.next
+                self.head = n
+            return data
+
+    def del_from_tail(self):
+        # delete the item from the tail of the list and return the data
+        # This is the only tricky method in the bunch. One has to setup
+        # two pointers, nth and nextToNth and walk them down to the end.
+        # Once nth is at the end, nextToNth is one away from the end and
+        # becomes the new tail.
+        if self.list_count > 0:
+            data = self.tail.data
+            self.list_count -= 1
+            if self.head.next == None:
+                self.head = None
+                self.tail = None
+            else:
+                nextToNth = self.head
+                nth = self.head.next
+                while nth.next != None:
+                    nextToNth = nth
+                    nth = nth.next
+                nextToNth.next = None
+                self.tail = nextToNth
+            return data
+
+class StackLL:
+    '''
+    ###############################################################################\n
+    # Stack Class implemented using Linked Lists\n
+    # Notes:\n
+    # __str__ returns a string representation of the entire list; useful for debug\n
+    # push adds the data to the tail of the list\n
+    # pop returns data from the tail of the list\n
+    # peek returns data from the tail of the list\n
+    ###############################################################################\n
+    '''
+    def __init__(self):
+        self.ll = LinkedList()
+
+    def __str__(self):
+        return f"{str(self.ll)}"
+
+    def is_empty(self):
+        return self.ll.list_count == 0
+
+    def push(self, item):
+        self.ll.add_to_tail(item)
+
+    def pop(self):
+        return self.ll.del_from_tail()
+
+    def peek(self):
+        return self.ll.peek_tail()
+
+    def size(self):
+        return self.ll.get_count()
+
 class Stack:
+    '''
+    ###############################################################################\n
+    # Stack Class implemented using Python Lists\n
+    # Notes:\n
+    # __str__ returns a string representation of the entire list; useful for debug\n
+    # push adds the data to the tail of the list\n
+    # pop returns data from the tail of the list\n
+    # peek returns data from the tail of the list\n
+    ###############################################################################\n
+    '''
+
     def __init__(self):
         self.items = []
 
@@ -20,3 +170,69 @@ class Stack:
 
     def size(self):
         return len(self.items)
+
+
+class FifoLL:
+    '''
+    ###############################################################################\n
+    # Fifo Class implemented using Linked Lists\n
+    # Notes:\n
+    # __str__ returns a string representation of the entire list; useful for debug\n
+    # push adds the data to the tail of the list\n
+    # pop returns data from the head of the list\n
+    # peek returns data from the head of the list\n
+    ###############################################################################\n
+    '''
+    def __init__(self):
+        self.ll = LinkedList()
+
+    def __str__(self):
+        return f"Fifo: {str(self.ll)}"
+
+    def is_empty(self):
+        return self.ll.get_count() == 0
+
+    def push(self, item):
+        self.ll.add_to_tail(item)
+
+    def pop(self):
+        return self.ll.del_from_head()
+
+    def peek(self):
+        return self.ll.peek_head()
+
+    def size(self):
+        return self.ll.get_count()
+
+class Fifo:
+    '''
+    ###############################################################################\n
+    # Fifo Class implemented using Python Lists\n
+    # Notes:\n
+    # __str__ returns a string representation of the entire list; useful for debug\n
+    # push adds the data to the tail of the list\n
+    # pop returns data from the head of the list\n
+    # peek returns data from the head of the list\n
+    ###############################################################################\n
+    '''
+    def __init__(self):
+        self.items = []
+
+    def __str__(self):
+        return f"Fifo: {str(tuple(self.items))}"
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop(0)
+
+    def peek(self):
+        return self.items[0]
+
+    def size(self):
+        return len(self.items)
+
--- testing/test_basic.py
+++ testing/test_basic.py
@@ -1,12 +1,38 @@
 import unittest
-from pythonds.basic import Stack
+from pythonds.basic import StackLL, Stack, FifoLL, Fifo
 
 class BasicTest(unittest.TestCase):
     def setUp(self):
         print("===========================================================")
-        print("self._testMethodName")
+        print(self._testMethodName)
 
-    # basic run thru most of the functions
+    # basic run thru most of the StackLL functions
+    def test_StackLL(self):
+        # Create the Stack
+        s = StackLL()
+        for i in range(11):
+            s.push(i)
+        self.assertEqual(s.peek(), 10, f'Value not matching: received {s.peek()} expected 10')
+        self.assertEqual(s.is_empty(), False, "Value not matching: received empty when expected not empty")
+        self.assertEqual(s.size(), 11, f"Value not matching: received size {s.size()} expected 11")
+        print(f"Stack Contents: {str(s)}")
+        for i in range(5):
+            v = s.pop()
+            expect = 10 - i
+            self.assertEqual(expect, v, f'Value not matching: received {v} expected {expect}')
+        self.assertEqual(s.peek(), 5, f'Value not matching: received {s.peek()} expected 5')
+        self.assertEqual(s.is_empty(), False, "Value not matching: received empty when expected not empty")
+        self.assertEqual(s.size(), 6, f"Value not matching: received size {s.size()} expected 6")
+        print(f"Stack Contents: {str(s)}")
+        for i in range(6):
+            v = s.pop()
+            expect = 5 - i
+            self.assertEqual(expect, v, f'Value not matching: received {v} expected {expect}')
+        self.assertEqual(s.is_empty(), True, "Value not matching: received not empty when expected empty")
+        self.assertEqual(s.size(), 0, f"Value not matching: received size {s.size()} expected 0")
+        print(f"Stack Contents: {str(s)}")
+
+    # basic run thru most of the Stack functions
     def test_Stack(self):
         # Create the Stack
         s = Stack()
@@ -31,6 +57,59 @@ class BasicTest(unittest.TestCase):
         self.assertEqual(s.is_empty(), True, "Value not matching: received not empty when expected empty")
         self.assertEqual(s.size(), 0, f"Value not matching: received size {s.size()} expected 0")
         print(f"Stack Contents: {str(s)}")
+
+    # basic run thru most of the FifoLL functions
+    def test_FifoLL(self):
+        # Create the Stack
+        s = FifoLL()
+        for i in range(11):
+            s.push(i)
+        self.assertEqual(s.peek(), 0, f'Value not matching: received {s.peek()} expected 10')
+        self.assertEqual(s.is_empty(), False, "Value not matching: received empty when expected not empty")
+        self.assertEqual(s.size(), 11, f"Value not matching: received size {s.size()} expected 11")
+        print(f"Fifo Contents: {str(s)}")
+        for i in range(5):
+            v = s.pop()
+            expect = i
+            self.assertEqual(expect, v, f'Value not matching: received {v} expected {expect}')
+        self.assertEqual(s.peek(), 5, f'Value not matching: received {s.peek()} expected 5')
+        self.assertEqual(s.is_empty(), False, "Value not matching: received empty when expected not empty")
+        self.assertEqual(s.size(), 6, f"Value not matching: received size {s.size()} expected 6")
+        print(f"Fifo Contents: {str(s)}")
+        for i in range(6):
+            v = s.pop()
+            expect = 5 + i
+            self.assertEqual(expect, v, f'Value not matching: received {v} expected {expect}')
+        self.assertEqual(s.is_empty(), True, "Value not matching: received not empty when expected empty")
+        self.assertEqual(s.size(), 0, f"Value not matching: received size {s.size()} expected 0")
+        print(f"Fifo Contents: {str(s)}")
+
+    # basic run thru most of the Fifo functions
+    def test_Fifo(self):
+        # Create the Stack
+        s = Fifo()
+        for i in range(11):
+            s.push(i)
+        self.assertEqual(s.peek(), 0, f'Value not matching: received {s.peek()} expected 10')
+        self.assertEqual(s.is_empty(), False, "Value not matching: received empty when expected not empty")
+        self.assertEqual(s.size(), 11, f"Value not matching: received size {s.size()} expected 11")
+        print(f"Fifo Contents: {str(s)}")
+        for i in range(5):
+            v = s.pop()
+            expect = i
+            self.assertEqual(expect, v, f'Value not matching: received {v} expected {expect}')
+        self.assertEqual(s.peek(), 5, f'Value not matching: received {s.peek()} expected 5')
+        self.assertEqual(s.is_empty(), False, "Value not matching: received empty when expected not empty")
+        self.assertEqual(s.size(), 6, f"Value not matching: received size {s.size()} expected 6")
+        print(f"Fifo Contents: {str(s)}")
+        for i in range(6):
+            v = s.pop()
+            expect = 5 + i
+            self.assertEqual(expect, v, f'Value not matching: received {v} expected {expect}')
+        self.assertEqual(s.is_empty(), True, "Value not matching: received not empty when expected empty")
+        self.assertEqual(s.size(), 0, f"Value not matching: received size {s.size()} expected 0")
+        print(f"Fifo Contents: {str(s)}")
+
 
 if __name__ == "__main__":
     unittest.main()
